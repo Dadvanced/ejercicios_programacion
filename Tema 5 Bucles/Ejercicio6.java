0e84ab390fd73e52a457b43f4946d73611b49dca
David León Galisteo <dadvanced@hotmail.es>
2016-10-26 08:51:50 +0200

    añadidio ejercicio 6

---
+++ Tema 5 Bucles/Ejercicio6.java
@@ -0,0 +1,24 @@
+/*
+*
+* @author David León Galisteo
+*
+* Ejercicio 6 tema 5 - bucles
+*
+*/
+
+public class Ejercicio6 {
+  public static void main(String[] args) {
+    System.out.println("\nSe va a mostrar una cuenta hacia atrás desde el");
+    System.out.println("320 hasta el 160 de 20 en 20");
+    System.console().readLine();
+    
+    int i = 320;
+    
+    do {
+      System.out.println("" + i);
+      i = (i-20);
+    }
+    while (i >=160);
+    
+  }
+}
