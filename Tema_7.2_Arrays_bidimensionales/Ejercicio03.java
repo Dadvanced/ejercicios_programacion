ef7953e183199105aaae4e3562996ea5d745ede5
David León Galisteo <dadvanced@hotmail.com>
2016-12-19 12:28:09 +0100

    añadido ejercicio 03

---
+++ Tema_7.2_Arrays_bidimensionales/Ejercicio03.java
@@ -0,0 +1,59 @@
+/*
+*
+* @author David León Galisteo
+* Ejercicio 03  - Tema 7.2
+*
+*/
+
+
+public class Ejercicio03 {
+  public static void main(String[] args) {
+    System.out.println("Se va a mostrar el sumatorio de un array de números aleatorios");
+    System.out.println("comprendidos entre el 100 y ell 999.");
+    System.out.println("_______________________________________________________________");
+    
+    
+    int[][] num = new int[4][5];
+    int fila = 0;
+    int columna = 0;
+    int sumaFila = 0;
+    int filaTotal = 0;
+    int columnaTotal = 0;
+    int sumaColumna = 0;
+    
+        //////////////generamos los números de manera aleatoria ////////////7
+    for (fila = 0; fila < 4; fila++) { 
+      for (columna = 0; columna < 5; columna++) {
+        num[fila][columna] = (int)(Math.random()*900) + 100;
+      }
+    }
+    System.out.println(); //salto de línea
+    
+    for (fila = 0; fila < 4; fila++) {
+      for (columna = 0; columna < 5; columna++) {
+        System.out.printf("%7d   ", num[fila][columna]);
+        sumaFila += num[fila][columna];
+      }
+      
+      System.out.printf("|%7d   ", sumaFila);
+      filaTotal += sumaFila;
+      sumaFila = 0;
+      System.out.println();
+    }
+    
+    System.out.println("_________________________________________________________________");
+    
+    for (columna = 0; columna < 5; columna++) {
+      for (fila = 0; fila < 4; fila++) {
+        sumaColumna += num[fila][columna];
+      }
+      
+      System.out.printf("%7d   ", sumaColumna);
+      columnaTotal += sumaColumna;
+      sumaColumna = 0;
+    }//for 
+    
+    System.out.printf("|%7d   ", (columnaTotal + filaTotal));
+    
+  }
+}
